a98cbd925619f255a214dd326a46d568ac73a53a
-THE-LAST-SAMURAI- <connor813@outlook.com>
2022-01-07 15:05:42 -0600

    almost finished main content articles

--- src/components/Content.js
+++ src/components/Content.js
@@ -13,10 +13,58 @@ function Content() {
             </div>
 
             <div className="main_articles_container">
+
+                <div className="article_one">
+                   
+                    <div>  
+                        <h1> TRUTH </h1>
+                        <h2> Trump's social network may launch on February 21 — and it's gonna be YUGE</h2> 
+                    </div>                 
+
+                </div>
+
+                <div className="article_two">
+
+                     <div>  
+                        <h1> BETTER THAN ZOOM </h1>
+                        <h2> How metaverse avatars could level the playing field for remote workers </h2> 
+                    </div>  
+
+                </div>
+
+                <div className="article_three">
+
+                    <div>  
+                        <h1> REVIEW </h1>
+                        <h2> The Aventon Soltera is a light ebike with tons of features for $1,200s </h2> 
+                    </div>  
+                                
+                </div>
+
+                <div className="article_four">
                 
+                    <div>  
+                        <h3> Mark your calendar for June 16 & 17! </h3>
+                        <h3> Join us at TNW Conference 2022 </h3>
+                        <button> GET TICKETS </button> 
+                    </div> 
+                
+                </div>
+
+                <div className="article_five">
+                
+                    <div>  
+                        <h1> LET'S BUY AN ISLAND </h1>
+                        <h2> Welcome to Cryptoland: The Fyre Festival for crypto fans </h2> 
+                    </div>   
+
+                </div>
+
             </div>
-       
-            <h4 style={{position: "absolute", marginTop:"120%"}}> down here </h4>
+
+
+
+            <h1 style={{postion: "absoulte", marginTop: "1200px"}}> down here </h1>    
         </div>
     )
 }
