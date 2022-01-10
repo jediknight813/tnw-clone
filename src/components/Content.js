1e8499b5a03aaeedc718181b8fafe73392eb4df0
-THE-LAST-SAMURAI- <connor813@outlook.com>
2022-01-10 14:32:57 -0600

    added latest articles and latest storys

--- src/components/Content.js
+++ src/components/Content.js
@@ -64,7 +64,165 @@ function Content() {
 
 
 
-            <h1 style={{postion: "absoulte", marginTop: "1200px"}}> down here </h1>    
+            <div>
+                <div className="ad_for_mobile">
+                    
+                    <div>  
+                        <h3> Mark your calendar for June 16 & 17! </h3>
+                        <h3> Join us at TNW Conference 2022 </h3>
+                        <button> GET TICKETS </button> 
+                    </div> 
+            
+                 </div>
+
+            </div>
+
+
+            <div className="popular_articles_today_container">
+                <h1> POPULAR ARTICLES TODAY </h1>
+
+
+                <div className="popular_articles_today_flexbox">
+
+                    <div className="popular_articles_today_flexbox_item">
+                        <h2>1</h2>
+                        <h3>Trump's social network may launch on February 21 — and it's gonna be YUGE</h3>
+                    </div>
+
+                    <div className="popular_articles_today_flexbox_item">
+                        <h2>2</h2>
+                        <h3>Think China will dominate the EV market? You're probably wrong</h3>
+                    </div>
+
+                    <div className="popular_articles_today_flexbox_item">
+                        <h2>3</h2>
+                        <h3>VC funds are just like Ponzi schemes — and you should take advantage of that when raising capital</h3>
+                    </div>
+
+                    <div className="popular_articles_today_flexbox_item">
+                        <h2>4</h2>
+                        <h3>Welcome to Cryptoland: The Fyre Festival for crypto fans</h3>
+                    </div>
+
+                    <div className="popular_articles_today_flexbox_item">
+                        <h2>5</h2>
+                        <h3>Your brand needs a Discord community — here's how to build it</h3>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+            <div className="center_latest_items">
+                <div className="latest_articles_container">
+
+                    <div className="latest_articles_container_header_container">
+                        Latest stories
+                    </div>
+
+
+                        <div className="latest_articles_container_article_flexbox">
+
+                            <div className="latest_articles_container_article_flexbox_item">
+                                <img alt="computer" src="https://img-cdn.tnwcdn.com/image?fit=285%2C150&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2022%2F01%2Fheader-image-macbook-pro-16.jpg&signature=82fbbf6ef4039f2171854a9df972a687"/>
+
+                                <div>
+                                    <h1> PLUGGED </h1>
+                                    <h2>The MacBook Pro turned 16 and it's finally good again </h2>
+                                </div>
+                            
+                            </div>
+
+                            <div className="latest_articles_container_article_flexbox_item">
+
+                                <img alt="computer" src="https://img-cdn.tnwcdn.com/image?fit=285%2C150&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2022%2F01%2Fteam-gq.jpeg&signature=138dfedef195b5858b6c5b18cdcf6109"/>
+
+                                <div>
+                                    <h1> GROWTH QUARTER </h1>
+                                    <h2>Entrepreneurs! Your social networks should be small and curated</h2>
+                                </div>
+
+                            </div>
+
+                            <div className="latest_articles_container_article_flexbox_item">
+
+                                <img alt="computer" src="https://img-cdn.tnwcdn.com/image?fit=285%2C150&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2022%2F01%2FUntitled-design-45-2.jpg&signature=f42784d2b4a4388633f3be452fe275fe"/>
+
+                                <div>
+                                    <h1> SHIFT </h1>
+                                    <h2> Here's why EV batteries sometimes burst into flames </h2>
+                                </div>
+
+                            </div>
+
+                            <div className="latest_articles_container_article_flexbox_item">
+                                
+                                <img alt="computer" src="https://img-cdn.tnwcdn.com/image?fit=285%2C150&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2022%2F01%2Frsz_1screenshot_2022-01-10_at_131316-1.jpeg&signature=7d55070ef8da19fd5223ad86f7578bcc"/>
+
+                                <div>
+                                    <h1> SHIFT </h1>
+                                    <h2>Skateboarding is thriving, electrically</h2>
+                                </div>
+
+                            </div>
+
+                            <div className="latest_articles_container_article_flexbox_item">
+                                
+                                <img alt="computer" src="https://img-cdn.tnwcdn.com/image?fit=285%2C150&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2022%2F01%2FGBUVR.jpg&signature=e6e36a58fdf0ee2ea8f7c8985d79954c"/>
+
+                                <div>
+                                    <h1> NEURAL </h1>
+                                    <h2>The metaverse could bring out the good, bad, and ugly sides of people</h2>
+                                </div>
+                            
+                            </div>
+
+                            <div className="latest_articles_container_article_flexbox_item">
+                            
+                                <img alt="computer" src="https://img-cdn.tnwcdn.com/image?fit=285%2C150&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2022%2F01%2FApple-Metaverse.jpg&signature=453d7f4d95b86edb7d858831adc711b0"/>
+
+                                <div>
+                                    <h1> PLUGGED </h1>
+                                    <h2>Apple doesn't care for the metaverse — and that's totally fine</h2>
+                                </div>
+                            
+                            </div>
+
+                            <div className="latest_articles_container_article_flexbox_item">
+                            
+                                <img alt="computer" src="https://img-cdn.tnwcdn.com/image?fit=285%2C150&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2022%2F01%2FCES-standards.jpg&signature=47e37b91826cf40b9814959634414537"/>
+
+                                <div>
+                                    <h1> PLUGGED </h1>
+                                    <h2>CES 2022 only managed to make tech standards messier and more confusing</h2>
+                                </div>
+                        
+                            </div>
+
+                            <div className="latest_articles_container_article_flexbox_item">
+                            
+                                <img alt="computer" src="https://img-cdn.tnwcdn.com/image?fit=285%2C150&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2022%2F01%2FDeath-in-space-hed.jpg&signature=052dacdaafb82855aae05275348e8b20"/>
+
+                                <div>
+                                    <h1> SPACE </h1>
+                                    <h2>What happens if a tourist dies in space?</h2>
+                                </div>
+                            
+                            </div>
+
+                        </div>
+
+                        <div className="latest_articles_container_button">
+                            <button> ALL NEWS </button>
+                        </div>
+
+
+                </div>
+            </div>
+
+
+            <h1 style={{postion: "absoulte", marginTop: "4200px"}}> down here </h1>    
         </div>
     )
 }
